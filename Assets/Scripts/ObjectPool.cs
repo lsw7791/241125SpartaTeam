9432d3444158c2cffab40860ae44b0084a30286a
BaekSeungWoo617 <koke100@naver.com>
2024-11-26 14:28:10 +0900

    Update ObjectPool.cs

--- Assets/Scripts/ObjectPool.cs
+++ Assets/Scripts/ObjectPool.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class ObjectPool : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // 풀에 저장할 오브젝트를 위한 변수
+    public GameObject objectPrefab;
+    public int poolSize = 10;
+    private Queue<GameObject> objectPool = new Queue<GameObject>();
+
     void Start()
     {
-        
+        // 풀 초기화
+        InitializePool();
     }
 
-    // Update is called once per frame
-    void Update()
+    // 오브젝트 풀 초기화
+    private void InitializePool()
     {
-        
+        for (int i = 0; i < poolSize; i++)
+        {
+            // 오브젝트를 생성하여 풀에 추가
+            GameObject obj = Instantiate(objectPrefab);
+            obj.SetActive(false); // 처음에는 비활성화
+            objectPool.Enqueue(obj);
+        }
+    }
+
+    // 풀에서 오브젝트 꺼내기
+    public GameObject GetObject()
+    {
+        if (objectPool.Count > 0)
+        {
+            // 풀에서 객체 꺼내기
+            GameObject obj = objectPool.Dequeue();
+            obj.SetActive(true); // 활성화
+            return obj;
+        }
+        else
+        {
+            // 풀에 객체가 없으면 새로운 객체 생성
+            GameObject obj = Instantiate(objectPrefab);
+            obj.SetActive(true);
+            return obj;
+        }
+    }
+
+    // 사용이 끝난 오브젝트를 풀로 반환
+    public void ReturnObject(GameObject obj)
+    {
+        obj.SetActive(false); // 비활성화
+        objectPool.Enqueue(obj); // 풀에 반환
     }
 }
