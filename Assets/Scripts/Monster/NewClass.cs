487ddf58b3d12996d898bab7d243f8835acd6819
lsw7791 <lsw7791@naver.com>
2024-11-26 16:26:29 +0900

    init : Monster.cs

---
+++ Assets/Scripts/Monster/NewClass.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    public string MonsterName { get; private set; }
+    public int Health { get; private set; }
+    public float Speed { get; private set; }
+    public int Damage { get; private set; }
+
+    public void Initialize(MonsterData data)
+    {
+        MonsterName = data.Name;
+        Health = data.Health;
+        Speed = data.Speed;
+        Damage = data.Damage;
+    }
+}
