9d02b0dae27b036d6f4d97b901046983e552b738
tkql0 <k1122tkql0@gmail.com>
2024-11-28 19:09:40 +0900

    chore : test용 주석

--- Assets/Scripts/Monster/MonsterController.cs
+++ Assets/Scripts/Monster/MonsterController.cs
@@ -42,7 +42,8 @@ public class MonsterController : MonoBehaviour
         monster.isDie = true;
     }
 
-    // TODO :: TestTrigger
+    // TODO :: TestTrigger // 이 트리거는 지우고 플레이어 측에서 공격 시 몬스터를 참조하고 
+    // MonsterController가 있는지 TryGetComponent로 확인 후 이벤트로 데미지 감소
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.TryGetComponent<TopDownController>(out var outPlayer))
