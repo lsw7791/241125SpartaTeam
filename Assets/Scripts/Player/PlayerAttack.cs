9386a4799f5cd873885799485ad3d1b93c82c111
BaekSeungWoo617 <koke100@naver.com>
2024-11-28 15:51:56 +0900

    feat: 공격 효과

---
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    enum AttackType
+    {
+        MiningTool,
+        SmallWP,
+        MediumWP,
+        BigWP,
+        Arrow,
+        Staff
+    }
+
+}
