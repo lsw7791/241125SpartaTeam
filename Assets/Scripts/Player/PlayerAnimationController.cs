ee32ff1c5aa7644a07aaeb66010b95b6aeffdde5
lsw7791 <lsw7791@naver.com>
2024-12-02 20:01:42 +0900

    init : 플레이어 공격, 방어 애니메이션

--- Assets/Scripts/Player/PlayerAnimationController.cs
+++ Assets/Scripts/Player/PlayerAnimationController.cs
@@ -3,18 +3,43 @@ using UnityEngine;
 public class PlayerAnimationController : MonoBehaviour
 {
     private Animator _animator;
+    [SerializeField] private GameObject equippedWeapon; // 현재 장착된 무기
+
 
     void Start()
     {
         _animator = GetComponent<Animator>();
+        equippedWeapon = null;
     }
 
     public void SetMoveAnimation(bool isMoving)
     {
-        _animator.SetBool("1_Move", isMoving);
+        _animator.SetBool("Move", isMoving);
     }
     public void TriggerAttackAnimation()
     {
-        _animator.SetTrigger("2_Attack");
+        // 장착된 무기가 없거나 태그가 설정되지 않은 경우 기본값으로 "Attack_Melee" 사용
+        string weaponTag = equippedWeapon?.tag ?? "Melee";
+
+        switch (weaponTag)
+        {
+            case "Bow":
+                _animator.SetTrigger("Attack_Bow");
+                break;
+            case "Staff":
+                _animator.SetTrigger("Attack_Staff");
+                break;
+            default:
+                _animator.SetTrigger("Attack_Melee");
+                break;
+        }
+    }
+    public void SetPaddingAnimation(bool isPadding)
+    {
+        _animator.SetBool("Padding", isPadding);
+    }
+    public void SetEquippedWeapon(GameObject weapon)
+    {
+        equippedWeapon = weapon; // 장착 무기 설정
     }
 }
