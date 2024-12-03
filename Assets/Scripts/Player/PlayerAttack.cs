be45134306600ad28fd04cb3f2b5b0598af25230
lsw7791 <lsw7791@naver.com>
2024-12-03 21:00:10 +0900

    init : EquipManager 생성

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -4,5 +4,24 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-   
+    [SerializeField] private GameObject _weapon; // 현재 무기를 나타내는 오브젝트
+    private SpriteRenderer _weaponSpriteRenderer; // _weapon 오브젝트의 SpriteRenderer
+
+    [SerializeField] private GameObject _newWeapon; // 새로 장착할 무기 프리팹
+
+    private void Awake()
+    {
+        _weapon = this.gameObject;
+        _weaponSpriteRenderer = _weapon.GetComponent<SpriteRenderer>(); // weapon의 SpriteRenderer 가져오기
+    }
+    
+    // 새로운 무기를 장착하고 weapon의 스프라이트를 교체
+    public void EquipWeapon(GameObject newWeaponPrefab)
+    {
+        _newWeapon = newWeaponPrefab; // 새로운 무기 오브젝트 설정
+        SpriteRenderer newWeaponSpriteRenderer = _newWeapon.GetComponent<SpriteRenderer>();
+
+        // weapon의 스프라이트를 새로운 무기의 스프라이트로 교체
+        _weaponSpriteRenderer.sprite = newWeaponSpriteRenderer.sprite;
+    }
 }
