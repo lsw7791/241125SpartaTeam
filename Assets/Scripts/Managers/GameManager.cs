28bf6aeef306399f5cfc718cd9f448e2a3c480fa
BaekSeungWoo617 <koke100@naver.com>
2024-12-03 15:04:36 +0900

    chore: 게임 매니저 스크립트,몬스터 생성 완료

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,17 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using MainData;
-using Tripolygon.UModeler.UI.ViewModels;
-using UnityEngine.UIElements;
 
 public class GameManager : MonoSingleton<GameManager>
 {
     [SerializeField] private GameObject goblinPrefab;  // 고블린 프리팹
-    [SerializeField] private GameObject lizardPrefab;  
-    [SerializeField] private GameObject zombiePrefab;  
-    [SerializeField] private GameObject orcShamanPrefab;  
-    [SerializeField] private GameObject BigzombiePrefab;  
+    [SerializeField] private GameObject lizardPrefab;
+    [SerializeField] private GameObject zombiePrefab;
+    [SerializeField] private GameObject orcShamanPrefab;
+    [SerializeField] private GameObject BigzombiePrefab;
     [SerializeField] private GameObject ImpPrefab;
     [SerializeField] private GameObject skeletPrefab;
     [SerializeField] private GameObject iceZombiePrefab;
@@ -20,21 +15,8 @@ public class GameManager : MonoSingleton<GameManager>
     [SerializeField] private GameObject necromancerPrefab;
     [SerializeField] private GameObject demonPrefab;
 
+    private MonsterPool monsterPool;
 
-    private MonsterPool goblinPool;
-    private MonsterPool lizardPool;
-    private MonsterPool zombiePool;
-    private MonsterPool orcShamanPool;
-    private MonsterPool bigZombiePool;
-    private MonsterPool impPool;
-    private MonsterPool skeletonPool;
-    private MonsterPool iceZombiePool;
-    private MonsterPool ogrePool;
-    private MonsterPool knightPool;
-    private MonsterPool necromancerPool;
-    private MonsterPool demonPool;
-
-    [SerializeField]
     protected override void Awake()
     {
         base.Awake();
@@ -50,82 +32,43 @@ public class GameManager : MonoSingleton<GameManager>
         knightPrefab = Resources.Load<GameObject>("Prefabs/Monsters/Knight");
         necromancerPrefab = Resources.Load<GameObject>("Prefabs/Monsters/Necromancer");
         demonPrefab = Resources.Load<GameObject>("Prefabs/Monsters/Demon");
-
     }
+
     private void Start()
     {
-
         DataManager.Instance.Initialize();
 
-        // Use JaSon Data To user Item information
-        List<ItemInstance> items = new List<ItemInstance>();
-        {
-        items.Add(new ItemInstance { id = 1, itemid = 1, itemtype=(ItemType)1, itemname ="실험1" });
-        }
-        ItemManager.Instance.Initialize(items);
+        monsterPool = new MonsterPool();
+
+        // 몬스터 풀 초기화 (여기서 풀을 생성합니다)
+        monsterPool.InitializeMonsterPool("Goblin", goblinPrefab, 5, 1);
+        monsterPool.InitializeMonsterPool("Lizard", lizardPrefab, 5, 2);
+        monsterPool.InitializeMonsterPool("Zombie", zombiePrefab, 5, 3);
+        monsterPool.InitializeMonsterPool("OrcShaman", orcShamanPrefab, 5, 4);
+        monsterPool.InitializeMonsterPool("BigZombie", BigzombiePrefab, 5, 5);
+        monsterPool.InitializeMonsterPool("Imp", ImpPrefab, 5, 6);
+        monsterPool.InitializeMonsterPool("Skelet", skeletPrefab, 5, 7);
+        monsterPool.InitializeMonsterPool("IceZombie", iceZombiePrefab, 5, 8);
+        monsterPool.InitializeMonsterPool("Ogre", ogrePrefab, 5, 9);
+        monsterPool.InitializeMonsterPool("Knight", knightPrefab, 5, 10);
+        monsterPool.InitializeMonsterPool("Necromancer", necromancerPrefab, 5, 11);
+        monsterPool.InitializeMonsterPool("Demon", demonPrefab, 5, 12);
 
 
-        // 몬스터 풀 초기화
-        goblinPool = new MonsterPool();
-        goblinPool.InitializeMonsterPool("Goblin", goblinPrefab, 2, 1);  // 고블린 풀 초기화
 
-        lizardPool = new MonsterPool();
-        lizardPool.InitializeMonsterPool("Lizard", lizardPrefab, 2, 2);  // 도마뱀 풀 초기화
-
-        zombiePool = new MonsterPool();
-        zombiePool.InitializeMonsterPool("Zombie", zombiePrefab, 2, 3);  // 좀비 풀 초기화
-
-        orcShamanPool = new MonsterPool();
-        orcShamanPool.InitializeMonsterPool("OrcShaman", orcShamanPrefab, 2, 4);  // 오크 샤먼 풀 초기화
-
-        bigZombiePool = new MonsterPool();
-        bigZombiePool.InitializeMonsterPool("BigZombie", BigzombiePrefab, 2, 5);  // 큰 좀비 풀 초기화
-
-        impPool = new MonsterPool();
-        impPool.InitializeMonsterPool("Imp", ImpPrefab, 2, 6);  // 임프 풀 초기화
-
-        skeletonPool = new MonsterPool();
-        skeletonPool.InitializeMonsterPool("Skeleton", skeletPrefab, 2, 7);  // 스켈레톤 풀 초기화
-
-        iceZombiePool = new MonsterPool();
-        iceZombiePool.InitializeMonsterPool("IceZombie", iceZombiePrefab, 2, 8);  // 아이스 좀비 풀 초기화
-
-        ogrePool = new MonsterPool();
-        ogrePool.InitializeMonsterPool("Ogre", ogrePrefab, 2, 9);  // 오우거 풀 초기화
-
-        knightPool = new MonsterPool();
-        knightPool.InitializeMonsterPool("Knight", knightPrefab, 2, 10);  // 나이트 풀 초기화
-
-        necromancerPool = new MonsterPool();
-        necromancerPool.InitializeMonsterPool("Necromancer", necromancerPrefab, 2, 11);  // 네크로맨서 풀 초기화
-
-        demonPool = new MonsterPool();
-        demonPool.InitializeMonsterPool("Demon", demonPrefab, 5, 12);  // 데몬 풀 초기화
-        Debug.Log("SpawnMonster전");
-
-        // 예시로 고블린 몬스터를 생성
-        //SpawnMonster(goblinPool, new Vector2(1f, 1f));
+        // 예시로 고블린 몬스터를 생성하고, 위치를 설정합니다
+        SpawnMonster("Goblin", new Vector2(1f, 1f));
+        SpawnMonster("Zombie", new Vector2(3f, 3f));
     }
 
-    void SpawnMonster(MonsterPool pool, Vector2 position)
+    void SpawnMonster(string monsterType, Vector2 position)
     {
-        Debug.Log("SpawnMonster 호출됨");
-        Debug.Log("Pool name: " + pool.name);  // pool.name 확인
 
-        GameObject monster = pool.GetMonster(pool.name);  // 풀에서 몬스터 가져오기
-
+        // 몬스터를 풀에서 가져옵니다
+        GameObject monster = monsterPool.GetMonster(monsterType, position);
         if (monster != null)
         {
-            Debug.Log("몬스터 생성됨: " + monster.name);  // 몬스터 생성 성공 여부 확인
-            monster.transform.position = position;  // 지정된 위치에 몬스터 배치
+
         }
-        else
-        {
-            Debug.LogError("몬스터를 풀에서 가져오지 못했습니다.");
-        }
-    }
-    void ReturnGoblin(GameObject goblin)
-    {
-        goblinPool.ReturnMonster("Goblin", goblin);  // 고블린을 풀에 반환
     }
 }
--- Assets/Scripts/ObjectPool/MonsterPool.cs
+++ Assets/Scripts/ObjectPool/MonsterPool.cs
@@ -1,11 +1,11 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using MainData;
 
 public class MonsterPool : MonoBehaviour
 {
     private Dictionary<string, Queue<GameObject>> monsterPools = new Dictionary<string, Queue<GameObject>>();
+    private Dictionary<string, int> poolSizes = new Dictionary<string, int>();  // 각 몬스터 타입의 초기 풀 크기 저장
+    private const int MAX_POOL_SIZE = 20;  // 풀의 최대 크기 제한
 
     // 풀 초기화 (데이터와 함께 초기화)
     public void InitializeMonsterPool(string monsterType, GameObject prefab, int poolSize, int creatureId)
@@ -28,10 +28,11 @@ public class MonsterPool : MonoBehaviour
         }
 
         monsterPools[monsterType] = poolQueue;
+        poolSizes[monsterType] = poolSize;  // 해당 몬스터의 풀 크기 저장
     }
 
-    // 몬스터를 요청하는 함수
-    public GameObject GetMonster(string monsterType)
+    // 몬스터를 요청하는 함수 (위치 매개변수 추가)
+    public GameObject GetMonster(string monsterType, Vector2 position)
     {
         if (monsterPools.ContainsKey(monsterType) && monsterPools[monsterType].Count > 0)
         {
@@ -45,13 +46,25 @@ public class MonsterPool : MonoBehaviour
                 monsterData.ResetStatus();  // 풀에서 반환된 몬스터 상태 리셋
             }
 
+            monsterObj.transform.position = position;  // 위치 설정
+
             return monsterObj;
         }
         else
         {
-            // 풀에 몬스터가 없으면 새로 생성 (성능 최적화를 위해 최소한의 로드를 고려할 수 있음)
-            GameObject newMonster = Instantiate(Resources.Load(monsterType) as GameObject);
-            return newMonster;
+            // 풀에 몬스터가 없고, 최대 풀 크기를 초과하지 않는 경우에만 새로운 몬스터 생성
+            if (!monsterPools.ContainsKey(monsterType) || monsterPools[monsterType].Count < MAX_POOL_SIZE)
+            {
+                GameObject newMonster = Instantiate(Resources.Load(monsterType) as GameObject);
+                newMonster.transform.position = position;  // 위치 설정
+
+                return newMonster;
+            }
+            else
+            {
+                Debug.LogWarning("Max pool size reached. Unable to create new monster.");
+                return null;  // 풀 크기 초과 시 null을 반환
+            }
         }
     }
 
