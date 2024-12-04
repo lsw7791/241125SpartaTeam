573d836fc71e9225ead11d5cfa274d79f1fbafc9
lsw7791 <lsw7791@naver.com>
2024-12-04 11:02:59 +0900

    init : 플레이어 죽음 애니메이션

--- Assets/Scripts/Player/TopDownController.cs
+++ Assets/Scripts/Player/TopDownController.cs
@@ -8,14 +8,13 @@ public class TopDownController : MonoBehaviour
     private Vector2 moveInput; // 이동 입력값
     private Rigidbody2D rb;
     private Camera camera;
-    private UnitRoot unitRoot;
     private TopDownAimRotation topDownAimRotation;
     private PlayerAnimationController _playerAnimationController;
+    private bool isDeath = false;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        unitRoot = GetComponentInChildren<UnitRoot>();
         topDownAimRotation =GetComponent<TopDownAimRotation>();
         _playerAnimationController = GetComponentInChildren<PlayerAnimationController>();
         camera = Camera.main;
@@ -24,6 +23,7 @@ public class TopDownController : MonoBehaviour
     // 이동 처리
     public void OnMove(InputAction.CallbackContext context)
     {
+        if (isDeath) return;
         moveInput = context.ReadValue<Vector2>();
         bool isMoving = moveInput.sqrMagnitude > 0; // 벡터 크기로 이동 여부 판단
         _playerAnimationController.SetMoveAnimation(isMoving);
@@ -38,12 +38,18 @@ public class TopDownController : MonoBehaviour
             Vector2 mouseWorldPos = camera.ScreenToWorldPoint(mouseScreenPos); // 월드 좌표로 변환
             Vector2 direction =(Vector2)transform.position - mouseWorldPos;
             // 플레이어와 마우스 위치를 비교하여 좌우 반전
-            unitRoot.FlipRotation(mouseWorldPos);
+            _playerAnimationController.FlipRotation(mouseWorldPos);
             topDownAimRotation.RotateArm(direction);
         }
     }
+    public void TriggerDeath()
+    {
+        _playerAnimationController.isDeath = true;
+        speed = 0f;
+        _playerAnimationController.TriggerDeathAnimation(); // 죽음 애니메이션 실행
+        this.enabled = false;
 
-    
+    }
 
     private void FixedUpdate()
     {
