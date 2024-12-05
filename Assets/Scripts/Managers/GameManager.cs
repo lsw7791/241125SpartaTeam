3e8257e9b750dbf5742f190e69844c6ef687ed80
BaekSeungWoo617 <koke100@naver.com>
2024-12-05 20:46:50 +0900

    refactor: 게임메니저랑 몬스터 풀 정리

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -2,62 +2,16 @@ using UnityEngine;
 using System.Collections.Generic;
 public class GameManager : MonoSingleton<GameManager>
 {
-    [SerializeField] public GameObject goblinPrefab;  // 몬스터 프리팹
-    [SerializeField] public GameObject lizardPrefab;
-    [SerializeField] public GameObject zombiePrefab;
-    [SerializeField] public GameObject orcShamanPrefab;
-    [SerializeField] public GameObject BigzombiePrefab;
-    [SerializeField] public GameObject ImpPrefab;
-    [SerializeField] public GameObject skeletPrefab;
-    [SerializeField] public GameObject iceZombiePrefab;
-    [SerializeField] public GameObject ogrePrefab;
-    [SerializeField] public GameObject knightPrefab;
-    [SerializeField] public GameObject necromancerPrefab;
-    [SerializeField] public GameObject demonPrefab;
-
-    [SerializeField] public GameObject stoneMine; //광석 프리팹
-    [SerializeField] public GameObject copperMine;
-    [SerializeField] public GameObject ironMine;
-    [SerializeField] public GameObject goldMine;
-    [SerializeField] public GameObject platinumMine;
-    [SerializeField] public GameObject ignisMine;
-
-
     public MonsterPool monsterPool;// 몬스터 풀
     //public ProjectilePool projectilePool; // 발사체 풀
-
     protected override void Awake()
     {
         base.Awake();
-        // 몬스터 프리팹 로드
-        goblinPrefab = Resources.Load<GameObject>("Prefabs/Monsters/Goblin");
-        lizardPrefab = Resources.Load<GameObject>("Prefabs/Monsters/Lizard");
-        zombiePrefab = Resources.Load<GameObject>("Prefabs/Monsters/Zombie");
-        orcShamanPrefab = Resources.Load<GameObject>("Prefabs/Monsters/OrcShaman");
-        BigzombiePrefab = Resources.Load<GameObject>("Prefabs/Monsters/BigZombie");
-        ImpPrefab = Resources.Load<GameObject>("Prefabs/Monsters/Imp");
-        skeletPrefab = Resources.Load<GameObject>("Prefabs/Monsters/Skelet");
-        iceZombiePrefab = Resources.Load<GameObject>("Prefabs/Monsters/IceZombie");
-        ogrePrefab = Resources.Load<GameObject>("Prefabs/Monsters/Ogre");
-        knightPrefab = Resources.Load<GameObject>("Prefabs/Monsters/Knight");
-        necromancerPrefab = Resources.Load<GameObject>("Prefabs/Monsters/Necromancer");
-        demonPrefab = Resources.Load<GameObject>("Prefabs/Monsters/Demon");
-        
-        // 광석 프리팹 로드
-        stoneMine = Resources.Load<GameObject>("Prefabs/Mines/StoneMine");
-        copperMine = Resources.Load<GameObject>("Prefabs/Mines/CopperMine");
-        ironMine = Resources.Load<GameObject>("Prefabs/Mines/IronMine");
-        goldMine = Resources.Load<GameObject>("Prefabs/Mines/GoldMine");
-        platinumMine = Resources.Load<GameObject>("Prefabs/Mines/PlatinumMine");
-        ignisMine = Resources.Load<GameObject>("Prefabs/Mines/IgnisMine");
-
-
         DataManager.Instance.Initialize();
 
         List<ItemInstance> items = new List<ItemInstance>(); // JSON파일에 저장된 유저 아이템 정보를 읽어서 넘겨준다.
        
         ItemManager.Instance.Initialize(items);
-
     }
 
     private void Start()
@@ -84,14 +38,6 @@ public class GameManager : MonoSingleton<GameManager>
         monsterPool.InitializeMonsterPool(10, 5);  // Knight
         monsterPool.InitializeMonsterPool(11, 5);  // Necromancer
         monsterPool.InitializeMonsterPool(12, 5);  // Demon
-
-        //monsterPool.InitializeMonsterPool(13, stoneMine, 5);  // stoneMine
-        //monsterPool.InitializeMonsterPool(14, copperMine, 5);  // stoneMine
-        //monsterPool.InitializeMonsterPool(15, ironMine, 5);  // stoneMine
-        //monsterPool.InitializeMonsterPool(16, goldMine, 5);  // stoneMine
-        //monsterPool.InitializeMonsterPool(17, platinumMine, 5);  // stoneMine
-        //monsterPool.InitializeMonsterPool(18, ignisMine, 5);  // stoneMine
-
 
         SpawnMonster(1, new Vector2(1f, 1f));
         SpawnMonster(4, new Vector2(2f, 1f));
