a4e69c9afa1dc4ddd40e893bce559ff784a03543
tkql0 <96943505+tkql0@users.noreply.github.com>
2024-12-12 02:44:32 +0900

    test : 먹은 아이템이 장비 아이템인지 확인하는 조건 추가
    확인 필요

--- Assets/UGS.Generated/Scripts/MainData.ITemData.cs
+++ Assets/UGS.Generated/Scripts/MainData.ITemData.cs
@@ -29,9 +29,10 @@ namespace MainData
 
 /* Your Loaded Data Storage. */
     
-        public static Dictionary<int, ItemData> ItemDataMap = new Dictionary<int, ItemData>();  
-        public static List<ItemData> ItemDataList = new List<ItemData>();   
-
+        public static Dictionary<int, ItemData> ItemDataMap = new Dictionary<int, ItemData>();
+        // ID와 데이터
+        public static List<ItemData> ItemDataList = new List<ItemData>();
+        // 모든 아이템 데이터를 리스트 형태로 저장
         /// <summary>
         /// Get ItemData List 
         /// Auto Load
