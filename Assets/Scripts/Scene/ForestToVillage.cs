47b880ac0377b981b347b5f3fa06c18b545a62ef
BaekSeungWoo617 <koke100@naver.com>
2024-12-13 10:29:25 +0900

    chore: 씬 이동(마을, 숲)

--- Assets/Scripts/Scene/ForestToVillage.cs
+++ Assets/Scripts/Scene/ForestToVillage.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VillageToForest : MonoBehaviour
+public class ForestToVillage : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Player"))
         {
-            GameManager.Instance.LoadScene(GameManager.Instance.dataManager.scene.GetMapTo(1));       
+            GameManager.Instance.LoadScene(GameManager.Instance.dataManager.scene.GetMapTo(2));
         }
     }
 }
