3ea944fa393a0b42731f936dbb3327db9ff5e6fd
lsw7791 <lsw7791@naver.com>
2024-12-16 11:08:18 +0900

    init : 캐릭터 생성 씬

--- Assets/Scripts/UI/TitleUI.cs
+++ Assets/Scripts/UI/TitleUI.cs
@@ -3,9 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class TitleUI : MonoBehaviour
 {
-    public void GameStart()
+    public void StartGame()
     {
         Debug.Log("게임 시작");
-        SceneManager.LoadScene("InGameScene");
+        SceneManager.LoadScene("PlayerCreation");
     }
+    public void LoadGame()
+    {
+        Debug.Log("게임 불러오기");
+        SceneManager.LoadScene("InGameScene");
+
+    }
+
+
 }
