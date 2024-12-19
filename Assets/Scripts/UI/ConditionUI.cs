5c9f89af87cad2125b34024818e070afa35e1766
lsw7791 <lsw7791@naver.com>
2024-12-19 19:57:56 +0900

    chore : 컨디션 유아이 정상 작동

--- Assets/Scripts/UI/ConditionUI.cs
+++ Assets/Scripts/UI/ConditionUI.cs
@@ -1,18 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ConditionUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Slider _hp;       // 체력 슬라이더
+    [SerializeField] private Slider _stamina; // 스태미너 슬라이더
+
+    private void Start()
     {
-        
+        InitializeSliders();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        UpdateSliders();
+    }
+
+    // 슬라이더를 초기화하는 메서드
+    private void InitializeSliders()
+    {
+        var playerStats = GameManager.Instance.Player.stats;
+        if (playerStats == null) return;
+
+        _hp.maxValue = playerStats.MaxHP;
+        _hp.value = playerStats.CurrentHP;
+
+        _stamina.maxValue = playerStats.MaxStamina;
+        _stamina.value = playerStats.CurrentStamina;
+    }
+
+    // 슬라이더를 지속적으로 업데이트하는 메서드
+    private void UpdateSliders()
+    {
+        var playerStats = GameManager.Instance.Player.stats;
+        if (playerStats == null) return;
+
+        _hp.value = playerStats.CurrentHP;
+        _stamina.value = playerStats.CurrentStamina;
     }
 }
