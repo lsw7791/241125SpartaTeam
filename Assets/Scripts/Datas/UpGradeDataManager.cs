253cebe48cc263cd3ad705c1dbb9737421ed0f55
tkql0 <96943505+tkql0@users.noreply.github.com>
2024-12-23 01:13:34 +0900

    test : 강화가 제대로 작동하는지 테스트
    하드코딩 + 가중치가 아닌 Ramdom.Range 사용

--- Assets/Scripts/Datas/UpGradeDataManager.cs
+++ Assets/Scripts/Datas/UpGradeDataManager.cs
@@ -1,11 +1,11 @@
 using MainData;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class UpGradeDataManager : UpGradeData
 {
-    // 특정 ID의 아이템 데이터를 반환
     public UpGradeData GetData(int id)
     {
         if (UpGradeDataMap.ContainsKey(id))
@@ -13,11 +13,5 @@ public class UpGradeDataManager : UpGradeData
             return UpGradeDataMap[id];
         }
         return null;
-    }
-
-    // 특정 수치와 티어에 해당하는 데이터를 반환
-    public UpGradeData GetUpgradeData(int inEnhence, int inTier)
-    {
-        return UpGradeDataList.Find(data => data.enhence == inEnhence && data.tier == inTier);
     }
 }
--- Assets/Scripts/UI/UpGradeUI.cs
+++ Assets/Scripts/UI/UpGradeUI.cs
@@ -1,25 +1,32 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UpGradeUI : UIBase
 {
     [SerializeField] private Image _useItemImage;
-    
-    [SerializeField] private Button _craftResultButton; // 조합 버튼
+
+    [SerializeField] private Button _upgradeResultButton; // 강화 버튼
+
+    [SerializeField] private TMP_Text _productText;
+    [SerializeField] private TMP_Text[] _probabilityText;
+
+    [SerializeField] private TMP_Text _ResultTestText;
+    [SerializeField] private TMP_Text _costText;
 
     InventoryItem item;
 
     private void Start()
     {
-        _craftResultButton.onClick.AddListener(() =>
+        _upgradeResultButton.onClick.AddListener(() =>
         {
-            ShowUpgradeChance(item, item.enhenceCount);
+            ShowUpgrade(item, item.enhenceCount);
         });
     }
 
-    public void ShowUpgradeChance(InventoryItem inItem, int inEnhence)
+    public void ShowUpgrade(InventoryItem inItem, int inEnhence)
     {
-        if (inItem.enhenceCount == 10)
+        if (inItem.enhenceCount > 10)
         {
             Debug.Log("최대 강화 수치입니다.");
             UIManager.Instance.ToggleUI<UpGradeUI>();
@@ -30,16 +37,37 @@ public class UpGradeUI : UIBase
         var itemData = GameManager.Instance.DataManager.GetItemDataById(inItem.ItemID);
 
         // 강화 데이터 검색
-        var upgradeData = GameManager.Instance.DataManager.Upgrade.GetUpgradeData(inEnhence, itemData.tier);
+        var upgradeData = GameManager.Instance.DataManager.Upgrade.GetData(inEnhence);
+
+        if (inItem.enhenceCount == 10)
+        {
+            Debug.Log("최대 강화 수치입니다.");
+            _productText.text = $"{itemData.name} (+Max)";
+            _probabilityText[0].text = $"성공 확률\n0%";
+            _probabilityText[1].text = $"유지 확률\n0%";
+            _probabilityText[2].text = $"파괴 확률\n0%";
+            _ResultTestText.text = "최고 레벨 입니다";
+            _costText.text = $"강화 비용: 0";
+            return;
+        }
 
         // 강화 확률 표시
-        Debug.Log($"아이템: {itemData.name} (티어 {itemData.tier})");
-        Debug.Log($"강화 단계: {inEnhence}");
-        Debug.Log($"성공 확률: {upgradeData.success}%");
-        Debug.Log($"실패 확률: {upgradeData.fail}%");
-        Debug.Log($"파괴 확률: {upgradeData.Destruction}%");
 
-        inItem.enhenceCount++;
+        _productText.text = $"{itemData.name} (+{inEnhence})";
+        _probabilityText[0].text = $"성공 확률\n{upgradeData.success[itemData.tier]}%";
+        _probabilityText[1].text = $"유지 확률\n{upgradeData.fail[itemData.tier]}%";
+        _probabilityText[2].text = $"파괴 확률\n{upgradeData.Destruction[itemData.tier]}%";
+        _costText.text = $"강화 비용: {upgradeData.Cost[itemData.tier]}";
+        int a = Random.Range(0, 100); // TODO :: 가중치로 바꾸기 전 테스트용 랜덤 값
+        if (upgradeData.success[itemData.tier] > a)
+        {
+            inItem.enhenceCount++;
+            _ResultTestText.text = "성공";
+        }
+        else
+        {
+            _ResultTestText.text = "실패";
+        }
     }
 
     public void Initialize(InventoryItem inItem)
@@ -47,7 +75,14 @@ public class UpGradeUI : UIBase
         item = inItem;
 
         var itemData = GameManager.Instance.DataManager.GetItemDataById(inItem.ItemID);
+        var upgradeData = GameManager.Instance.DataManager.Upgrade.GetData(inItem.enhenceCount);
 
         _useItemImage.sprite = inItem.ItemIcon;
+        _productText.text = $"{itemData.name} (+{inItem.enhenceCount})";
+        _probabilityText[0].text = $"성공 확률\n{upgradeData.success[itemData.tier]}%";
+        _probabilityText[1].text = $"유지 확률\n{upgradeData.fail[itemData.tier]}%";
+        _probabilityText[2].text = $"파괴 확률\n{upgradeData.Destruction[itemData.tier]}%";
+        _ResultTestText.text = "결과";
+        _costText.text = $"강화 비용: {upgradeData.Cost[itemData.tier]}";
     }
 }
