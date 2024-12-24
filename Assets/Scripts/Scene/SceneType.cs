f12770caabe1fbb6ea30fcfc89e4d489eed3b5f7
BaekSeungWoo617 <koke100@naver.com>
2024-12-24 16:39:54 +0900

    chore: 빌리지 안에 집에 들어가면 이제 씬이동은 안합니다

--- Assets/Scripts/Scene/SceneType.cs
+++ Assets/Scripts/Scene/SceneType.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneType : MonoBehaviour
 {
@@ -10,7 +11,19 @@ public class SceneType : MonoBehaviour
         if(collision.CompareTag("Player"))
         {
             GameManager.Instance.SceneNum = num;
+            string currentSceneName = SceneManager.GetActiveScene().name;//현재 씬 이름 가져오기
+            if(currentSceneName != GameManager.Instance.DataManager.Scene.GetMapTo(GameManager.Instance.SceneNum))
+            {
             GameManager.Instance.LoadScene(GameManager.Instance.DataManager.Scene.GetMapTo(GameManager.Instance.SceneNum));
+            }
+            else
+            {
+                Transform playerTransform = collision.transform; // 충돌한 플레이어의 Transform 가져오기
+                playerTransform.position = GameManager.Instance.DataManager.Scene.GetMoveTransform(num);
+                Camera mainCamera = Camera.main;
+                mainCamera.transform.position = playerTransform.position;
+                GameManager.Instance.miniCamera.transform.position = playerTransform.position;
+            }
         }
     }
 }
