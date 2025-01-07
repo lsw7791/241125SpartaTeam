46f5ce8fd14c67991d2807d6fc7eb5c7289ed7fe
lsw7791 <lsw7791@naver.com>
2025-01-07 20:55:29 +0900

    init : 인트로씬 애니메이션

--- Assets/Scripts/Quest/QuestIcon.cs
+++ Assets/Scripts/Quest/QuestIcon.cs
@@ -1,14 +1,27 @@
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;  
 
 public class QuestIcon : UIBase
 {
+    public float scaleDuration = 1f;  
+    public float scaleFactor = 1.5f;  
     // 클릭 시 실행될 메서드
     public void OnQuestIconClick()
     {
-        Debug.Log("Quest Icon Clicked!");  // 클릭 확인을 위한 로그
-        this.gameObject.SetActive(false);  // 클릭 시 아이콘 비활성화
+        this.gameObject.SetActive(false);  
         UIManager.Instance.ToggleUI<MainQuestUI>();
+    }
 
+    private void Start()
+    {
+        StartScaling();
+    }
+
+    private void StartScaling()
+    {
+        transform.DOScale(scaleFactor, scaleDuration)  // 크기 변경 애니메이션
+                 .SetLoops(-1, LoopType.Yoyo)          // Yoyo는 애니메이션을 반복하면서, 커졌다가 작아지는 효과
+                 .SetEase(Ease.InOutSine);            // 부드러운 easing 효과
     }
 }
