11c9ace7db0807d9ca212c252cade3f2435a21fd
tkql0 <96943505+tkql0@users.noreply.github.com>
2025-01-13 03:11:45 +0900

    chore : 플레이어가 아이템 일정 범위에 있다면 자석 효과 주기

---
+++ Assets/Scripts/Creature/ItemMagnet.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemMagnet : MonoBehaviour
+{
+    private float _speed;
+    private float _magnetDistance;
+
+    private Transform _TatgetPosition;
+
+    private void OnEnable()
+    {
+        _TatgetPosition = GameManager.Instance.Player.transform;
+    }
+
+    private void Start()
+    {
+        _speed = 2f;
+        _magnetDistance = 5f;
+    }
+
+    private void OnDestroy()
+    {
+        // 맵을 넘어갈 때 혹시 모를 버그를 위해 초기화
+        _TatgetPosition = null;
+    }
+
+    private void Update()
+    {
+        if (!_TatgetPosition)
+        { // 타겟이 초기화 되어있다면 리턴
+            return;
+        }
+
+        float targetToDistance = Vector3.Distance(transform.position, _TatgetPosition.position);
+
+        if(targetToDistance <= _magnetDistance)
+        {
+            transform.position = Vector3.MoveTowards
+                (transform.position, _TatgetPosition.position, _speed * Time.deltaTime);
+        }
+    }
+}
